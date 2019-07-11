9dba7cb0456dc117797c239dd139a62e25dc8d2c
William Welling <wwelling@library.tamu.edu>
2019-07-11 13:10:41 -0500

    batch person documents, comments of concern

--- src/main/java/edu/tamu/scholars/middleware/graphql/resolver/EnhancedPersonResolver.java
+++ src/main/java/edu/tamu/scholars/middleware/graphql/resolver/EnhancedPersonResolver.java
@@ -1,5 +1,6 @@
 package edu.tamu.scholars.middleware.graphql.resolver;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,6 +21,8 @@ import io.leangen.graphql.spqr.spring.annotations.GraphQLApi;
 @GraphQLApi
 public class EnhancedPersonResolver {
 
+    private final static int MAX_DOCUMENT_BATCH_SIZE = 500;
+
     @Autowired
     private PersonService personService;
 
@@ -29,14 +32,34 @@ public class EnhancedPersonResolver {
     @GraphQLQuery(name = "person")
     public EnhancedPerson getById(@GraphQLArgument(name = "id") String id) {
         Person person = personService.getById(id);
-        List<String> ids = person.getPublications().stream().map(document -> document.getId()).collect(Collectors.toList());
-        List<Document> selectedPublications = documentService.findByIdIn(ids);
         EnhancedPerson enhancedPerson = new EnhancedPerson();
-        enhancedPerson.setSelectedPublications(selectedPublications);
+        enhancedPerson.setSelectedPublications(getPersonsPublications(person));
         BeanUtils.copyProperties(person, enhancedPerson);
         return enhancedPerson;
     }
 
+    // NOTE: batch requests by ids to avoid exceeding Solr query string limit
+    private List<Document> getPersonsPublications(Person person) {
+        List<Document> selectedPublications = new ArrayList<Document>();
+        List<String> ids = person.getPublications().stream().map(document -> document.getId()).collect(Collectors.toList());
+        while (ids.size() >= MAX_DOCUMENT_BATCH_SIZE) {
+            selectedPublications.addAll(documentService.findByIdIn(ids.subList(0, MAX_DOCUMENT_BATCH_SIZE)));
+            ids = ids.subList(MAX_DOCUMENT_BATCH_SIZE, ids.size());
+        }
+        selectedPublications.addAll(documentService.findByIdIn(ids));
+        return selectedPublications;
+    }
+
+    // NOTE: This concrete class extending the generated nested Person class is required
+    // to generate the GraphQL schema in which to support return type with a Person and 
+    // a list of full publications. Also, undesired is the additional list of publications.
+    // Unfortunately, there is no way to override the existing property accessor methods 
+    // with a different generic for the return list.
+    // To remedy this, we could annotate the flatten Solr document models with information
+    // on how to generate nested documents with complete relational nested documents. There
+    // will have to be a naming convention used.
+    // I would also like to have annotations on the flatten Solr document models specify
+    // the variable name more appropriate than label of the partial nested objects.
     @GraphQLType(name = "EnhancedPerson")
     public class EnhancedPerson extends Person {
 
