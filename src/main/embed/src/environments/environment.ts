749c9d58242087439e36e6c3b1b8a9d79b6b300b
William Welling <wwelling@library.tamu.edu>
2019-08-13 13:49:06 -0500

    removed embed client

--- src/main/embed/src/environments/environment.ts
+++
@@ -1,9 +0,0 @@
-export const environment = {
-    serviceUrl: 'http://localhost:9000',
-    uiUrl: 'http://localhost:3000',
-    vivoUrl: 'https://scholars.library.tamu.edu/vivo',
-    formalize: {
-        otherUniversity: 'ExternalOrganization',
-        GreyLiterature: 'InstitutionalRepositoryDocument'
-    }
-};
