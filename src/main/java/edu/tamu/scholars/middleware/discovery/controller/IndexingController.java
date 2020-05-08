62c0cb050755e8d2a3f6d877a3a40cbc594f39a5
William Welling <wwelling@library.tamu.edu>
2020-05-08 08:32:43 -0500

    add simple endpoint to check if indexing or not

---
+++ src/main/java/edu/tamu/scholars/middleware/discovery/controller/IndexingController.java
@@ -0,0 +1,25 @@
+package edu.tamu.scholars.middleware.discovery.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.tamu.scholars.middleware.discovery.service.IndexService;
+
+@RestController
+@RequestMapping("/index")
+public class IndexingController {
+
+  @Autowired
+  private IndexService indexService;
+
+  @GetMapping("/isIndexing")
+  public ResponseEntity<Boolean>  isIndexing() {
+    Boolean isIndexing = indexService.isIndexing();
+    return isIndexing ? ResponseEntity.status(HttpStatus.ACCEPTED).body(isIndexing) : ResponseEntity.ok(isIndexing);
+  }
+  
+}
