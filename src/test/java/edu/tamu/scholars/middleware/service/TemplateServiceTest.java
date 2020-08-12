617ec0453a4bbef3aec99c0365b52107b7182240
William Welling <wwelling@library.tamu.edu>
2020-08-12 11:35:17 -0500

    normalize templates for test assertions

--- src/test/java/edu/tamu/scholars/middleware/service/TemplateServiceTest.java
+++ src/test/java/edu/tamu/scholars/middleware/service/TemplateServiceTest.java
@@ -4,8 +4,9 @@ import static edu.tamu.scholars.middleware.auth.RegistrationTestUtility.getMockR
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.File;
-import java.nio.charset.Charset;
+import java.nio.charset.StandardCharsets;
 
+import org.apache.commons.lang3.StringUtils;
 import org.assertj.core.util.Files;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -42,14 +43,16 @@ public class TemplateServiceTest {
     public void testTemplateConfirmRegistrationMessage() {
         ReflectionTestUtils.setField(templateService, "uiUrl", "http://localhost:4200");
         Registration registration = getMockRegistration("Bob", "Boring", "bboring@mailinator.com");
-        String message = templateService.templateConfirmRegistrationMessage(registration, "MTU0NTg1NTI3MDMzMzo3YWFiMWRlYjNkNTRkM2M0NGMxYmI4ODZjNDIyMjEzYzk2NjBkOGIxNDgwMmJjNGY0YjBkYzdiMmE4YWVkMWRmOnsiZmlyc3ROYW1lIjoiQm9iIiwibGFzdE5hbWUiOiJCb3JpbmciLCJlbWFpbCI6ImJib3JpbmdAbWFpbGluYXRvci5jb20ifTpkOTAyZjNlZWZhMDJlYzRiM2M0NDE3ZDFiYjYwMmI5ZTlkYWUxODU1OGUzNGExN2I5NWI1NDdjYjNmODIxZmNmYWZhNDA3ZDI0NmUwOWQ2YTAyMmJmZGI2OGUzODkwNzlhNDRhNjVmZTMyNjlmY2M1M2FlOTUzM2U0ZTE2ZDE4Yg==");
-        assertEquals(Files.contentOf(new File("src/test/resources/mock/templates/email/confirm-registration.html"), Charset.defaultCharset()), message);
+        String actual = templateService.templateConfirmRegistrationMessage(registration, "MTU0NTg1NTI3MDMzMzo3YWFiMWRlYjNkNTRkM2M0NGMxYmI4ODZjNDIyMjEzYzk2NjBkOGIxNDgwMmJjNGY0YjBkYzdiMmE4YWVkMWRmOnsiZmlyc3ROYW1lIjoiQm9iIiwibGFzdE5hbWUiOiJCb3JpbmciLCJlbWFpbCI6ImJib3JpbmdAbWFpbGluYXRvci5jb20ifTpkOTAyZjNlZWZhMDJlYzRiM2M0NDE3ZDFiYjYwMmI5ZTlkYWUxODU1OGUzNGExN2I5NWI1NDdjYjNmODIxZmNmYWZhNDA3ZDI0NmUwOWQ2YTAyMmJmZGI2OGUzODkwNzlhNDRhNjVmZTMyNjlmY2M1M2FlOTUzM2U0ZTE2ZDE4Yg==");
+        String expected = Files.contentOf(new File("src/test/resources/mock/templates/email/confirm-registration.html"), StandardCharsets.UTF_8);
+        assertEquals(StringUtils.normalizeSpace(expected), StringUtils.normalizeSpace(actual));
     }
 
     @Test
     public void testTemplateSparql() {
-        String query = templateService.templateSparql("collection", "http://xmlns.com/foaf/0.1/Person");
-        assertEquals(Files.contentOf(new File("src/test/resources/mock/templates/sparql/collection.sparql"), Charset.defaultCharset()), query);
+        String actual = templateService.templateSparql("collection", "http://xmlns.com/foaf/0.1/Person");
+        String expected = Files.contentOf(new File("src/test/resources/mock/templates/sparql/collection.sparql"), StandardCharsets.UTF_8);
+        assertEquals(StringUtils.normalizeSpace(expected), StringUtils.normalizeSpace(actual));
     }
 
 }
